01602403be0bce1dfd60312f1fb72d20fe6b718e
Civitaspo <civitaspo@gmail.com>
2019-10-07 23:44:49 +0900

    Close pooler by injector

--- src/main/scala/pro/civitaspo/digdag/plugin/pg_lock/pg/PgLockPgConnectionPoolerProvider.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/pg_lock/pg/PgLockPgConnectionPoolerProvider.scala
@@ -3,6 +3,7 @@ package pro.civitaspo.digdag.plugin.pg_lock.pg
 
 import com.google.inject.{Inject, Provider}
 import io.digdag.client.config.Config
+import javax.annotation.PreDestroy
 
 
 class PgLockPgConnectionPoolerProvider
@@ -10,8 +11,20 @@ class PgLockPgConnectionPoolerProvider
 {
     @Inject protected var systemConfig: Config = null
 
+    lazy private val pooler: PgLockPgConnectionPooler =
+        new PgLockPgConnectionPooler(config = PgLockPgConfig(systemConfig))
+
     override def get(): PgLockPgConnectionPooler =
     {
-        new PgLockPgConnectionPooler(config = PgLockPgConfig(systemConfig))
+        pooler
+    }
+
+
+    @PreDestroy
+    def shutdown(): Unit =
+    {
+        pooler.synchronized {
+            pooler.shutdown()
+        }
     }
 }
